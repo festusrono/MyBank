2409fc22994eed920d4ace9a38a012ec0dceaef0
festusrono <festusrono56@gmail.com>
2025-03-29 13:09:23 +0100

    currencies structure

--- app/src/main/java/com/example/mybank/CurrenciesSection.kt
+++ app/src/main/java/com/example/mybank/CurrenciesSection.kt
@@ -1,9 +1,26 @@
 package com.example.mybank
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.material.icons.rounded.CurrencyYen
+import androidx.compose.material.icons.rounded.Euro
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mybank.data.Currency
+import java.lang.reflect.Modifier
 
 val currencies = listOf(
      Currency(
@@ -16,7 +33,46 @@ val currencies = listOf(
          name = "EUR",
          buy = 23.35f,
          sell = 23.25f,
-         icon = Icons.Rounded.CurrencyYen
+         icon = Icons.Rounded.Euro
      ),
 
+    Currency(
+        name = "USD",
+        buy = 23.35f,
+        sell = 23.25f,
+        icon = Icons.Rounded.AttachMoney
+    ),
+    Currency(
+        name = "EUR",
+        buy = 23.35f,
+        sell = 23.25f,
+        icon = Icons.Rounded.Euro
+    ),
+    Currency(
+        name = "YEN",
+        buy = 23.35f,
+        sell = 23.25f,
+        icon = Icons.Rounded.CurrencyYen
+    )
  )
+@Preview
+@Composable
+fun CurrenciesSection() {
+    var isVisible by remember {
+        mutableStateOf(false)
+    }
+    var iconState by remember {
+        mutableStateOf(Icons.Rounded.KeyboardArrowUp)
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+        ) {  }
+    }
+}
