68762c5ab0b2131b092439eee85e93700cfe71d1
festusrono <festusrono56@gmail.com>
2025-03-29 11:38:15 +0100

    Icons dependencies extended

--- app/src/main/java/com/example/mybank/CurrenciesSection.kt
+++ app/src/main/java/com/example/mybank/CurrenciesSection.kt
@@ -1,2 +1,22 @@
 package com.example.mybank
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AttachMoney
+import androidx.compose.material.icons.rounded.CurrencyYen
+import com.example.mybank.data.Currency
+
+val currencies = listOf(
+     Currency(
+         name = "USD",
+         buy = 23.35f,
+         sell = 23.25f,
+         icon = Icons.Rounded.AttachMoney
+     ),
+     Currency(
+         name = "EUR",
+         buy = 23.35f,
+         sell = 23.25f,
+         icon = Icons.Rounded.CurrencyYen
+     ),
+
+ )
