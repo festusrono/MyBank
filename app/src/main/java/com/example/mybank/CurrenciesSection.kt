057be413bac82746f8ab6e2061dfa4e8d50d39de
festusrono <festusrono56@gmail.com>
2025-03-29 16:28:43 +0100

    currencies structure

--- app/src/main/java/com/example/mybank/CurrenciesSection.kt
+++ app/src/main/java/com/example/mybank/CurrenciesSection.kt
@@ -1,15 +1,31 @@
 package com.example.mybank
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.material.icons.rounded.CurrencyYen
 import androidx.compose.material.icons.rounded.Euro
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,8 +33,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mybank.data.Currency
 import java.lang.reflect.Modifier
 
@@ -65,14 +86,198 @@ fun CurrenciesSection() {
         mutableStateOf(Icons.Rounded.KeyboardArrowUp)
     }
     Box(
-        modifier = Modifier
+        modifier = androidx.compose.ui.Modifier
             .fillMaxSize()
             .padding(top = 32.dp),
         contentAlignment = Alignment.BottomCenter
     ) {
+
         Column(
-            modifier = Modifier
+            modifier = androidx.compose.ui.Modifier
                 .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
-        ) {  }
+                .background(MaterialTheme.colorScheme.inverseOnSurface)
+                .animateContentSize()
+        ) {
+            Row(
+                modifier = androidx.compose.ui.Modifier
+                    .padding(16.dp)
+                    .animateContentSize()
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Box(modifier = androidx.compose.ui.Modifier
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.secondary)
+                    .clickable {
+                        isVisible = !isVisible
+                        iconState = if (isVisible) {
+                            Icons.Rounded.KeyboardArrowUp
+                        } else {
+                            Icons.Rounded.KeyboardArrowDown
+                        }
+                    }
+                ) {
+                    Icon(
+                        modifier = androidx.compose.ui.Modifier.size(25.dp),
+                        imageVector = iconState,
+                        contentDescription = "Currencies",
+                        tint = MaterialTheme.colorScheme.onSecondary
+                    )
+                }
+
+                Spacer(modifier = androidx.compose.ui.Modifier.width(20.dp))
+
+                Text(
+                    text = "Currencies",
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
+
+            Spacer(
+                modifier = androidx.compose.ui.Modifier
+                    .height(1.dp)
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
+            )
+
+            if (isVisible) {
+                BoxWithConstraints(
+                    modifier = androidx.compose.ui.Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                        .clip(RoundedCornerShape(topStart = 25.dp, topEnd = 25.dp))
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+
+                    val boxWithConstraintsScope = this
+                    val width = boxWithConstraintsScope.maxWidth / 3
+
+                    Column(
+                        modifier = androidx.compose.ui.Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    ) {
+
+                        Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+
+                        Row(
+                            modifier = androidx.compose.ui.Modifier.fillMaxWidth()
+                        ) {
+
+                            Text(
+                                modifier = androidx.compose.ui.Modifier.width(width),
+                                text = "Currency",
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+
+
+                            Text(
+                                modifier = androidx.compose.ui.Modifier.width(width),
+                                text = "Buy",
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                textAlign = TextAlign.End
+                            )
+
+
+                            Text(
+                                modifier = androidx.compose.ui.Modifier.width(width),
+                                text = "Sell",
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                textAlign = TextAlign.End
+                            )
+                        }
+
+                        Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+
+                        LazyColumn {
+                            items(currencies.size) { index ->
+                                CurrencyItem(
+                                    index = index,
+                                    width = width
+                                )
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun CurrencyItem(index: Int, width: Dp) {
+    val currency = currencies[index]
+
+    Row(
+        modifier = androidx.compose.ui.Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Row(
+            modifier = androidx.compose.ui.Modifier.width(width),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = androidx.compose.ui.Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(GreenStart)
+                    .padding(4.dp)
+            ) {
+                Icon(
+                    modifier = androidx.compose.ui.Modifier.size(18.dp),
+                    imageVector = currency.icon,
+                    contentDescription = currency.name,
+                    tint = Color.White
+                )
+            }
+
+            Text(
+                modifier = androidx.compose.ui.Modifier
+                    .padding(start = 10.dp),
+                text = currency.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+        }
+
+        Text(
+            modifier = androidx.compose.ui.Modifier
+                .width(width)
+                .padding(start = 10.dp),
+            text = "$ ${currency.buy}",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.End
+        )
+
+        Text(
+            modifier = androidx.compose.ui.Modifier
+                .width(width)
+                .padding(start = 10.dp),
+            text = "$ ${currency.sell}",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.End
+        )
+
+    }
     }
 }
